c26e68597bd679dca11d6f57e41f0ce7589696d3
Ben Pritchard <ben@psagami.net>
2020-10-27 08:02:10 +1100

    Add ratel port into docker start up script

--- start-server.sh
+++ start-server.sh
@@ -6,6 +6,6 @@ if docker ps -a --format '{{.Names}}' | grep -Eq "^${DGRAPH_CONTAINER_NAME}\$"; 
   echo "Already running..."
 else
   echo "Starting local dgraph server via Docker..."
-  docker run --name $DGRAPH_CONTAINER_NAME --rm -p 9082:9080 -d dgraph/standalone:master
+  docker run --name $DGRAPH_CONTAINER_NAME --rm -p 9082:9080 -p 8082:8080 -p 8002:8000 -d dgraph/standalone:master
 fi
 echo "Done."
